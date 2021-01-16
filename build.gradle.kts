5b1ad9c0da1e6be98d9759c7c8fc747c5c9315d3
shinyay <shinya.com@gmail.com>
2021-01-16 22:03:22 +0900

    Add dependency for Cloud Trace

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ extra["springCloudVersion"] = "2020.0.0"
 
 dependencies {
 	implementation("com.google.cloud:spring-cloud-gcp-starter")
+	implementation("com.google.cloud:spring-cloud-gcp-starter-trace")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
