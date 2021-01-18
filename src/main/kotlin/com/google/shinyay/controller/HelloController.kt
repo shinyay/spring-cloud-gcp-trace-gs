26e52d6bddb5b4d12be778252a45b1eeb2f6dffd
shinyay <shinya.com@gmail.com>
2021-01-18 21:52:41 +0900

    Modify controller package

--- src/main/kotlin/com/google/shinyay/controller/HelloController.kt
+++ src/main/kotlin/com/google/shinyay/controller/HelloController.kt
@@ -1,15 +1,13 @@
-package controller
+package com.google.shinyay.controller
 
 import com.google.shinyay.logger
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 @RestController
-@RequestMapping("/api/v1")
 class HelloController {
 
     @GetMapping("/hello")
