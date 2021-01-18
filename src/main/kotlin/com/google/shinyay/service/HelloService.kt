183dff7d74ba0b5b2ef3b39f6f268a62c3d1824a
shinyay <shinya.com@gmail.com>
2021-01-18 22:03:30 +0900

    Add hello function

--- src/main/kotlin/com/google/shinyay/service/HelloService.kt
+++ src/main/kotlin/com/google/shinyay/service/HelloService.kt
@@ -1,7 +1,18 @@
 package com.google.shinyay.service
 
+import com.google.shinyay.logger
 import org.springframework.stereotype.Service
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @Service
 class HelloService {
+
+    fun hello(): String {
+        val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+        val currentTime = ZonedDateTime.now(ZoneId.of("Japan")).format(dateFormat)
+        logger.info("Current Time: $currentTime")
+        return "Hello at $currentTime"
+    }
 }
