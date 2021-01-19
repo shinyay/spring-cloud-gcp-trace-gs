763194aacc34df7088de5807d3711f181a92d9ce
shinyay <shinya.com@gmail.com>
2021-01-19 22:31:32 +0900

    Add function to call hello service

--- src/main/kotlin/com/google/shinyay/service/ClientService.kt
+++ src/main/kotlin/com/google/shinyay/service/ClientService.kt
@@ -1,7 +1,14 @@
 package com.google.shinyay.service
 
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForObject
 
 @Service
-class ClientService {
+class ClientService() {
+
+    fun callHelloService(): String? {
+        val result = RestTemplate().getForObject("http://localhost:8080/api/v1/hello", String::class.java)
+        return result
+    }
 }
