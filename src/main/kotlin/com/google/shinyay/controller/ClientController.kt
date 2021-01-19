d0b80ccb1e6bca78f1cdbc71aef7fa62b73f1a8d
shinyay <shinya.com@gmail.com>
2021-01-19 22:40:33 +0900

    Add class level api uri

--- src/main/kotlin/com/google/shinyay/controller/ClientController.kt
+++ src/main/kotlin/com/google/shinyay/controller/ClientController.kt
@@ -1,8 +1,11 @@
 package com.google.shinyay.controller
 
 import com.google.shinyay.service.ClientService
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1")
 class ClientController(val service: ClientService) {
+
 }
