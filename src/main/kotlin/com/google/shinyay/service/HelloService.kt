29e5779645925eb9f516370588a63b65a45a6d90
shinyay <shinya.com@gmail.com>
2021-01-20 22:06:37 +0900

    Add call susupend function with runBlocking

--- src/main/kotlin/com/google/shinyay/service/HelloService.kt
+++ src/main/kotlin/com/google/shinyay/service/HelloService.kt
@@ -2,6 +2,7 @@ package com.google.shinyay.service
 
 import com.google.shinyay.logger
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -12,6 +13,9 @@ import kotlin.random.Random
 class HelloService {
 
     fun hello(): String {
+        runBlocking {
+            blockFunction()
+        }
         val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
         val currentTime = ZonedDateTime.now(ZoneId.of("Japan")).format(dateFormat)
         logger.info("Current Time: $currentTime")
@@ -19,8 +23,6 @@ class HelloService {
     }
 
     suspend fun blockFunction() {
-        logger.info("Current Time: ${ZonedDateTime.now(ZoneId.of("Japan"))
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS))}"))}")
         val randomValue = Random.nextLong(1000, 2000)
         delay(randomValue)
         logger.info("Delay: $randomValue")
