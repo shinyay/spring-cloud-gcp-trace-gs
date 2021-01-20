8e32de8d122848fa1797e65e7265bc7e177638fd
shinyay <shinya.com@gmail.com>
2021-01-20 22:25:34 +0900

    Add New Span definition by NewSpan Annotation

--- src/main/kotlin/com/google/shinyay/service/ClientService.kt
+++ src/main/kotlin/com/google/shinyay/service/ClientService.kt
@@ -1,6 +1,7 @@
 package com.google.shinyay.service
 
 import com.google.shinyay.logger
+import org.springframework.cloud.sleuth.annotation.NewSpan
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import kotlin.random.Random
@@ -8,6 +9,7 @@ import kotlin.random.Random
 @Service
 class ClientService() {
 
+    @NewSpan
     fun callHelloService(): String? {
         val resultList = mutableListOf<String>()
         repeat(Random.nextInt(1, 5)) {
