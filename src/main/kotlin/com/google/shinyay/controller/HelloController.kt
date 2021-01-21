6a0d8c52fe9ed83e325cf3d7119f47cf41c9c966
shinyay <shinya.com@gmail.com>
2021-01-21 20:10:36 +0900

    Modify import to optimize

--- src/main/kotlin/com/google/shinyay/controller/HelloController.kt
+++ src/main/kotlin/com/google/shinyay/controller/HelloController.kt
@@ -1,13 +1,9 @@
 package com.google.shinyay.controller
 
-import com.google.shinyay.logger
 import com.google.shinyay.service.HelloService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.ZoneId
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 
 @RestController
 @RequestMapping("/api/v1")
