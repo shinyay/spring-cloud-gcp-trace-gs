a2d2044ee99c9e7c85839b2dfdf4e759f0d1f335
shinyay <shinya.com@gmail.com>
2021-01-21 20:24:34 +0900

    Add info log to display start time

--- src/main/kotlin/com/google/shinyay/service/ClientService.kt
+++ src/main/kotlin/com/google/shinyay/service/ClientService.kt
@@ -4,6 +4,9 @@ import com.google.shinyay.logger
 import org.springframework.cloud.sleuth.annotation.NewSpan
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import kotlin.random.Random
 
 @Service
@@ -11,6 +14,8 @@ class ClientService() {
 
     @NewSpan
     fun callHelloService(): String? {
+        logger.info("Start-ClientService: ${ZonedDateTime.now(ZoneId.of("Japan")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))}")
+
         val resultList = mutableListOf<String>()
         repeat(Random.nextInt(1, 5)) {
             resultList.add(RestTemplate().getForObject("http://localhost:8080/api/v1/hello", String::class.java).toString())
